04e4e8d6205e37945012cd008c883e861c8305ce
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-10-27 20:11:44 -0300

    async/await

---
+++ esnext/asyncAwait.js
@@ -0,0 +1,38 @@
+// com async await ...
+const http = require("http");
+
+const getTurma = (letra) => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`;
+    return new Promise((resolve, reject) => {
+        http.get(url, (res) => {
+            let resultado = "";
+
+            res.on("data", (dados) => {
+                resultado += dados;
+            });
+
+            res.on("end", () => {
+                try {
+                    resolve(JSON.parse(resultado));
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        });
+    });
+};
+
+// Recurso do ES8
+// Objetivo de simplificar o use de promises...
+let obterAlunos = async () => {
+    const turmaA = await getTurma("A");
+    const turmaB = await getTurma("B");
+    const turmaC = await getTurma("C");
+
+    return [].concat(turmaA, turmaB, turmaC);
+};
+
+obterAlunos()
+    .then((alunos) => alunos.map((aluno) => aluno.nome))
+    .then((nomes) => console.log(nomes))
+    .catch(e => console.log(e.message));
