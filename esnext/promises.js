19f5529cfb6b78ea97f88f4de5c0e5d6a150ebf5
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-10-27 18:08:00 -0300

    Promises

---
+++ esnext/promises.js
@@ -0,0 +1,12 @@
+function falarDepoisDe (segundo, frase) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(frase);
+        }, segundo * 1000);
+    });
+}
+
+falarDepoisDe(3, 'Que legal!')
+    .then(frase => frase.concat('?!?'))
+    .then(outraFrase => console.log(outraFrase))
+    .catch(e => console.log(e));
