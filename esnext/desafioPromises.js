70afba092dcd29ea2173cb4eb60017d0831e2f49
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-10-28 01:11:58 -0300

    challenge promise

---
+++ esnext/desafioPromises.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const caminho = __dirname + '/dados.txt';
+
+function lerArquivo (caminho) {
+    return new Promise(resolve => {
+        fs.readFile(caminho, 'utf-8', (err, conteudo) => {
+            resolve(conteudo.toString());
+        });
+    });
+}
+
+lerArquivo(caminho)
+    .then(conteudo => conteudo.split('\n'))
+    .then(linha => linha.join(','))
+    .then(conteudo => `O valor final eh: ${conteudo}`)
+    .then(console.log);
