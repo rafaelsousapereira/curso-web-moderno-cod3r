eba088238cbf794795728796a80ecbdb151e7c19
rafaelsousapereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-02-22 05:00:13 -0300

    feat: add calculator project

---
+++ vue/calculator/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <h1>Olá Vue!</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style>
+@font-face {
+  font-family: 'Roboto';
+  src: url("/./src/fonts/Roboto-Thin.ttf");
+}
+
+* {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  height: 100vh;
+
+  text-align: center;
+
+  color: white;
+
+  background: linear-gradient(to right, rgb(83,105,118), rgb(41, 46, 73));
+}
+</style>
